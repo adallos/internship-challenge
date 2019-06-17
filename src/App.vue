9cc28705d08edc80b06638b96d96ef7120acaab9
Ana Dallos <ana.dallos@endava.com>
2019-06-16 23:06:58 -0500

    Revert "fetching appts by chunks of 10. Not styled yet but functional"
    This reverts commit 510c3d6ed200c84e6396b939508f63dab684d1f8.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view/>
+    
   </div>
 </template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -14,15 +22,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
